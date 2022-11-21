01d5d28b82a34f55635d0a15b4e0870815903020
taozhen3 <tz_email@163.com>
2022-11-21 20:56:19 +0800

    Initial commit

--- testcase/test_send_requst.py
+++ testcase/test_send_requst.py
@@ -1,20 +1,24 @@
 # B站讲的最详细的Python接口自动化测试实战教程全集（实战最新版）
 # day1
-# p2 p3
+# p2 p3 p4 p5 p6 p7
 import json
 import pytest
 import requests
 import json
 
+
 class Test_login:
     def test_login(self):
         url = "https://boss.sdf.testxyy.cn/backApi/sxx/uas/UserBasicMS/bossInteraction/doLogin"
-        headers = {"utype":"boss","Content-Type": "application/json","EncryptType": "AES"}
+        headers = {"utype": "boss", "Content-Type": "application/json", "EncryptType": "AES"}
         data = {
             "userName": "amSRwwDvBGjfu638KBwAbA==",
             "password": "VO4AUibpUy2SUAdzYFm40Q=="
         }
-        rep = requests.post(url = url, data = json.dumps(data),headers = headers)
+        rep = requests.post(url=url, data=json.dumps(data), headers=headers)
         token = rep.json()['data']['authorization']
         print(token)
 
+
+if __name__ == "__main__":
+    pytest.main('-vs')
